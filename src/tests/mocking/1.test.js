6ce09270247cd5adddd01b6713ab1c4c082409ad
nikizaniki2 <nikizaniki2@gmail.com>
2021-09-03 11:29:18 +0300

    Now Using Jest Mocking Moved getWinner to its own file

--- src/tests/mocking/1.test.js
+++ src/tests/mocking/1.test.js
@@ -1,23 +1,17 @@
 /* eslint-disable no-func-assign */
-function getWinner(p1, p2) {
-  let winner_id =  Math.round(Math.random());
-  switch(winner_id){
-  case(0): return p1;
-  case(1): return p2;
-  default: return "No winner";
-  }
-}
+
+const myFunc = require('./getWinner');
 
 test('getWinner returns', () => {
-  // MOCKING
-  const originalGetWinner = getWinner;
-  getWinner = jest.fn((p1, p2) => p1);
+  // MOCKING with jest
+  jest.spyOn(myFunc ,'getWinner');
+  myFunc.getWinner.mockImplementation((p1, p2) => p1);
 
-  const result = getWinner("Player 1", "Player 2");
+  const result = myFunc.getWinner("Player 1", "Player 2");
   expect(result).toBe("Player 1");
 
-  expect(getWinner).toHaveBeenCalledWith("Player 1", "Player 2");
+  expect(myFunc.getWinner).toHaveBeenCalledWith("Player 1", "Player 2");
 
   //  undo MOCKING
-  getWinner = originalGetWinner;
+  myFunc.getWinner.mockRestore();
 });
